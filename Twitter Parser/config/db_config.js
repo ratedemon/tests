09503e7b278a9d74b5ad434986089c16b87b4b7d
dmitryi <rated_demon@ukr.net>
2018-06-25 02:13:32 +0300

    start twitter

---
+++ Twitter Parser/config/db_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'vHbQw8IX1piQHTKqhZBxVxx9W',
+    consumer_secret: 'F4BGpkg4hL0pSNHiy9EYtsinstLIkYAIx1pSTLlSwSbpJ4eaFg',
+    access_token_key: '379580412-SaUEgABdBNJU6i7YCAFsi8PN63XXvkKwXSjn5qvz',
+    access_token_secret: 'IFjCKtSTLgU4DcTUlAhXxDFXdV0hr0VVFLAdlZJpcBIsD'
+};
